49e62b77de2c58e15e7397313e75e71a32c4a599
dhruvvanjara <dvanjara@tulip-tech.com>
2023-06-16 01:11:17 +0530

    Added pop up

--- src/components/LandingPageComponent/index.tsx
+++ src/components/LandingPageComponent/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet';
 
 import LandingPageFooter from '../LandingPageFooter';
@@ -11,6 +11,32 @@ import { useConfigStore } from '#src/stores/ConfigStore';
 const LandingPageComponent: React.FC = () => {
   const { config } = useConfigStore(({ config }) => ({ config }));
   const { siteName } = config;
+  const [isOpen, setIsOpen] = useState(true);
+  const timeoutRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  timeoutRef.current = window.setTimeout(() => {
+    setIsOpen(false);
+  }, 5000);
+  const closePopup = () => {
+    setIsOpen(false);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  };
+
+  const handleOutsideClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target instanceof HTMLDivElement && event.target.id === 'popup-container') {
+      closePopup();
+    }
+  };
   return (
     <div className={styles.LandingPage}>
       <Helmet>
@@ -18,6 +44,19 @@ const LandingPageComponent: React.FC = () => {
         <meta property="og:title" content={siteName} />
         <meta name="twitter:title" content={siteName} />
       </Helmet>
+      {isOpen && (
+        <div id="popup-container" className={styles.popupContainer} onClick={handleOutsideClick}>
+          <div className={styles.popupContent} id="popup-content">
+            <button onClick={closePopup}>Close</button>
+            <p>
+              Due to scheduled video provider maintenance our Sunday evening games will not air at the following websites instead of gameinframe.com: Brooklyn
+              City FC vs Dutch Lions: Eleven, NY Braveheart vs NJ Alliance: Youtube. Head to our{' '}
+              <a href="https://www.instagram.com/gameinframe/">Instagram@gameinframe </a>
+              to get direct game links.
+            </p>
+          </div>
+        </div>
+      )}
       <LandingPageCarousel />
       <LandingPageAbout />
       <LandingPageFooter />
